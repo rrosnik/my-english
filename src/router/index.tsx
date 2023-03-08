b58c4c96e4af8f83fc45bbd2f1c1cd1f658fbf83
reza rostami nikoo <rezarostaminikoo@gmail.com>
2023-03-08 12:09:48 -0500

    update for netlify

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -25,7 +25,7 @@ const router = createBrowserRouter([
         ]
     },
 
-], { basename: "/my-english" });
+]);
 
 
 export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,4 @@ export default defineConfig({
   server: {
     host: true
   },
-  base:"/my-english"
 })
