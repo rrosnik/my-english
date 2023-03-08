2316a57852864906ab60557c38f996c6983507f2
reza rostami nikoo <rezarostaminikoo@gmail.com>
2023-03-08 15:21:38 -0500

    added PWA to the app

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+  VitePWA({
+    registerType: 'prompt',
+    includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+    manifest: {
+      name: 'Myglish',
+      short_name: 'ME',
+      description: 'An App for English Learning',
+      theme_color: '#ff0088',
+      start_url: '/',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+  })],
   server: {
     host: true
   },
