2d1f7b3cad4cdf24b8805539a4bbe3e7d294a3c1
reza rostami nikoo <rezarostaminikoo@gmail.com>
2023-03-14 22:11:59 -0400

    added close button to the side bar

--- src/layouts/AppLayout/index.tsx
+++ src/layouts/AppLayout/index.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, Outlet } from "react-router-dom";
 
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
 import { UserType } from '../../redux/reducers/userReducer';
@@ -12,9 +11,11 @@ import { Button, InputGroup, Offcanvas } from 'react-bootstrap';
 import apis from '../../apis';
 import EmailVerifyingPage from '../../pages/EmailVerifyingPage';
 
+import "./index.scss";
+
 const AppLayout = () => {
   const currentUser = useSelector<RootState, UserType | null>(state => state.user.currentUser);
-
+  const [show, setShow] = useState<boolean>(false);
   if (!currentUser?.emailVerified) {
     return (
       <EmailVerifyingPage />
@@ -22,7 +23,7 @@ const AppLayout = () => {
   }
 
   return (
-    <div>
+    <div className='layout'>
       {/* <!-- Navbar --> */}
       <Navbar bg="light" expand="sm" collapseOnSelect >
         {/* <!-- Container wrapper --> */}
@@ -31,30 +32,37 @@ const AppLayout = () => {
           <Navbar.Brand>My English</Navbar.Brand>
 
           {/* <!-- Toggle button --> */}
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Offcanvas id="basic-navbar-nav" placement="end">
+          <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={() => setShow(true)} />
+          <Navbar.Offcanvas id="basic-navbar-nav" placement="end" show={show}>
             <Offcanvas.Header>
               <Offcanvas.Title className='w-100'>
+                <div className='name'>
+                  My English
+                  <Button variant='danger' onClick={() => setShow(false)}>X</Button>
+                </div>
+
                 {
                   currentUser
                     ?
-                    <InputGroup className='w-100'>
+                    <InputGroup className='w-100 mt-4'>
                       <Button disabled variant="outline-secondary flex-grow-1 text-start">
                         {currentUser.displayName}
                       </Button>
                       <Button type="button" onClick={() => apis.auth.userSignedOut()}>Sign out</Button>
                     </InputGroup>
                     :
-                    <Nav className="me-auto">
+                    <Nav className="me-auto mt-4">
                       <Nav.Link as="div">
                         <Link to="sign-in">Sign in</Link>
                       </Nav.Link>
                     </Nav>
                 }
               </Offcanvas.Title>
+
             </Offcanvas.Header>
 
             <Offcanvas.Body>
+
               <Nav className="me-auto">
                 <Nav.Link as="div">
                   <Link to="collection">
@@ -88,37 +96,6 @@ const AppLayout = () => {
           </Navbar.Offcanvas>
         </Container>
       </Navbar>
-      {/* 
-        
-          
-            <Nav className="me-auto">
-              <Nav.Link as="div">
-                <Link to="insert">Insert</Link>
-              </Nav.Link>
-              <Nav.Link as="div">
-                <Link to="review">Review</Link>
-              </Nav.Link>
-            </Nav>
-
-            <Navbar.Text>
-              {
-                currentUser
-                  ?
-                  <InputGroup>
-                    <Button disabled variant="outline-secondary">
-                      {currentUser.displayName}
-                    </Button>
-                    <Button type="button" onClick={() => apis.auth.userSignedOut()}>Sign out</Button>
-                  </InputGroup>
-                  :
-                  <Nav className="me-auto">
-                    <Nav.Link as="div">
-                      <Link to="sign-in">Sign in</Link>
-                    </Nav.Link>
-                  </Nav>
-              }
-            </Navbar.Text>
-       */}
       <Container fluid>
         <Outlet />
       </Container>
