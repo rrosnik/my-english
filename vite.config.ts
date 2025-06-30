5af5d207b9b2df6e6dab130e5acbd34d2853d85a
Reza Rostaminikoo RezaRostaminikoo@gmail.com <RezaRostaminikoo@gmail.com>
2025-06-30 12:10:58 -0400

    add storry book to the project

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
+// import path from 'node:path';
+// import { fileURLToPath } from 'node:url';
+// import { storybookTest } from '@storybook/addon-vitest/vitest-plugin';
+// const dirname = typeof __dirname !== 'undefined' ? __dirname : path.dirname(fileURLToPath(import.meta.url));
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 export default defineConfig({
   plugins: [
     react(),
@@ -38,4 +45,32 @@ export default defineConfig({
   server: {
     host: true,
   },
+  // test: {
+  //   projects: [
+  //     {
+  //       extends: true,
+  //       plugins: [
+  //         // The plugin will run tests for the stories defined in your Storybook config
+  //         // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+  //         storybookTest({
+  //           configDir: path.join(dirname, '.storybook'),
+  //         }),
+  //       ],
+  //       test: {
+  //         name: 'storybook',
+  //         browser: {
+  //           enabled: true,
+  //           headless: true,
+  //           provider: 'playwright',
+  //           instances: [
+  //             {
+  //               browser: 'chromium',
+  //             },
+  //           ],
+  //         },
+  //         setupFiles: ['.storybook/vitest.setup.ts'],
+  //       },
+  //     },
+  //   ],
+  // },
 });
